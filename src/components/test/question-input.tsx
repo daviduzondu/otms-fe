cfe4f46632fb5bb04f2f3ee0cadc725346d54e28
David <uzondudavid2004@gmail.com>
2024-11-08 20:24:24 +0100

    updated roundness of question input box

--- src/components/test/question-input.tsx
+++ src/components/test/question-input.tsx
@@ -1,50 +1,56 @@
 'use client'
 
-import React, { useState, useRef, useCallback, useEffect, LegacyRef, forwardRef } from 'react'
+import React, {forwardRef, LegacyRef, useEffect} from 'react'
 import 'react-quill/dist/quill.snow.css'
 import katex from 'katex'
 import 'katex/dist/katex.min.css'
-import ReactQuill, { ReactQuillProps } from 'react-quill';  // Import directly
-window.katex = katex;
+import ReactQuill, {ReactQuillProps} from 'react-quill'; // Import directly
 import "@/app/globals.css"
+
+window.katex = katex;
 
 
 // Quill modules configuration
 const modules = {
 
- toolbar: [
-  ['bold', 'italic', 'underline', 'strike'],
-  [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-  ['link'],
-  ['clean']
- ],
- formula: true,
- clipboard: {
-  matchVisual: true,
- },
+    toolbar: [
+        ['bold', 'italic', 'underline', 'strike'],
+        [{'list': 'ordered'}, {'list': 'bullet'}],
+        ['link'],
+        ['clean']
+    ],
+    formula: true,
+    clipboard: {
+        matchVisual: true,
+    },
 }
 
 const formats = [
- 'header',
- 'bold', 'italic', 'underline', 'strike',
- 'list', 'bullet',
- 'link', 'image',
- 'formula'
+    'header',
+    'bold', 'italic', 'underline', 'strike',
+    'list', 'bullet',
+    'link', 'image',
+    'formula'
 ]
 
 
-function Component({ value, onChange }: ReactQuillProps, ref: LegacyRef<ReactQuill>) {
- return (
-  <ReactQuill
-   className='w-full h-full'
-   ref={ref}
-   value={value}
-   onChange={onChange}
-   modules={modules}
-   formats={formats}
-   theme="snow"
-  />
- )
+function Component({value, onChange}: ReactQuillProps, ref: LegacyRef<ReactQuill>) {
+    useEffect(() => {
+        document.querySelector(".ql-container")!.setAttribute("style", "border-radius: 0px 0px  calc(var(--radius) - 2px) calc(var(--radius) - 2px)")
+        document.querySelector(".ql-toolbar")!.setAttribute("style", "border-radius: calc(var(--radius) - 2px) calc(var(--radius) - 2px) 0px 0px")
+    }, []);
+
+    return (
+        <ReactQuill
+            className='w-full h-full'
+            ref={ref}
+            value={value}
+            onChange={onChange}
+            modules={modules}
+            formats={formats}
+            theme="snow"
+        />
+    )
 }
 
 const WYSIWYGLatexEditor = forwardRef(Component);
