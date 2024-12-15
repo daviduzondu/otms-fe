8cf44effb3bd979227a220dd6e941694c31f05f8
David <uzondudavid2004@gmail.com>
2024-12-15 20:51:38 +0100

    made changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- reactStrictMode: false,
- typescript: {
-  ignoreBuildErrors: true
- },
- eslint: {
-  ignoreDuringBuilds: true
- }
+    reactStrictMode: false,
+    typescript: {
+        ignoreBuildErrors: true
+    },
+    eslint: {
+        ignoreDuringBuilds: true
+    },
+    webpack: (
+        config,
+        {buildId, dev, isServer, defaultLoaders, nextRuntime, webpack}
+    ) => {
+        if (config.cache && !dev) {
+            config.cache = Object.freeze({
+                type: 'memory',
+            })
+        }
+        // Important: return the modified config
+        return config
+    },
 };
 
 export default nextConfig;
